294258228816e1cd0d1e5e8d3ccc7df203715cb4
lidiia <lgliducik@gmail.com>
2022-12-22 00:02:50 +0300

    added task10, task13,task15

---
+++ task13/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет. Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int FindThird(int num)
+{
+    int c = num;
+    while (c>999)
+    {
+        c = c / 10;
+        Console.WriteLine($"{c}");
+    }
+    return c % 10;
+}
+
+if (number < 100)
+{
+    Console.WriteLine("Цифра не 3х значная");
+}
+else
+{
+    int third = FindThird(number);
+    Console.WriteLine($"3-яя цифра: {third}");
+}
---
+++ task15/Program.cs
@@ -0,0 +1,24 @@
+// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите день недели(цифру):");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<1 || number>7)
+{
+    Console.WriteLine("Такого дня недели нет.");
+}
+else
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
