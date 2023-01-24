1cfa87353ecfd5fbb7f345e3ba819de62b55743f
lidiia <lgliducik@gmail.com>
2023-01-24 12:17:04 +0400

    add task41, task43

---
+++ task41/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("Введите сколько чисел будете вводить: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+string createInputArray(int size)
+{
+    string numbers = string.Empty;
+    for(int i = 0; i<size; i++)
+    {
+        numbers =  numbers + Console.ReadLine() + " ";
+    }
+    return numbers;
+}
+
+string [] parseInput = createInputArray(m).Split();
+
+int CountPositiveNumbers(string[] arr)
+{
+    int count = 0;
+    for(int i = 0; i<arr.Length; i++)
+    {
+        if (!String.IsNullOrWhiteSpace(arr[i]))
+        {
+            if( Convert.ToInt16(arr[i])>0)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+int countPositive = CountPositiveNumbers(parseInput);
+Console.WriteLine($"Количество чисел > 0 = {countPositive}");
---
+++ task43/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("k1 = ");
+double inputK1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("k2 = ");
+double inputK2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b1 = ");
+double inputB1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b2 = ");
+double inputB2 = Convert.ToInt32(Console.ReadLine());
+
+double[] FindCrossPoint(double k1, double k2, double b1, double b2)
+{
+    double[] res = new double[2];
+    res[0] = (b2-b1)/(k1-k2);
+    res[1] = k2* res[0] + b2;
+    return res;
+}
+
+double[] res = FindCrossPoint(inputK1, inputK2, inputB1, inputB2);
+Console.Write($"({res[0]}, {res[1]})");
