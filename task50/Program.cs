abe593ab12a88e8e0c2e1a18d3ca45931b06b33e
lidiia <lgliducik@gmail.com>
2023-01-27 11:44:46 +0400

    add task47, task50, task52

---
+++ task50/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1,7 -> такого элемента в массиве нет
+
+
+int[,] matrix = { { 0, 1, 2 }, { 3, 4, 5 } };
+
+Console.WriteLine("введите i = ");
+int inputI = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите j = ");
+int inputJ = Convert.ToInt32(Console.ReadLine());
+
+isExistElementIJ(matrix, inputI, inputJ);
+
+void isExistElementIJ(int[,] matrix, int i, int j)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    if (i >= 0 && j >= 0 && i < rows && j < columns)
+    {
+        Console.WriteLine($"{matrix[i,j]}");
+    }
+    else Console.WriteLine("Такого элемента нет");
+}
+
+
---
+++ task52/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 5},");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+double[] FindArithmeticAverages(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    double[] res = new double[columns];
+    for (int i = 0; i < columns; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            res[i] += matrix[j,i] ;
+        }
+        res[i] = Math.Round(res[i]/rows, 1); ;
+    }
+    return res;
+}
+
+void PrintArray(double []arr)
+{
+    Console.Write("[");
+    foreach (double element in arr)
+    {
+        Console.Write(element + " ");
+    }
+    Console.Write("]");
+}
+
+
+int[,] array2D = CreateMatrixRndInt(3,4,1,9);
+PrintMatrix(array2D);
+double[] res = FindArithmeticAverages(array2D);
+PrintArray(res);
